6fd54ff6419018e91f69d8ab5ce240e1e22f90c0
Caramelo18 <fabiocaramelo18@gmail.com>
2017-11-08 22:41:18 +0000

    First pages created

---
+++ FirstREST/Areas/SalesClient/SalesClientAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace FirstREST.Areas.SalesClient
+{
+    public class SalesClientAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "SalesClient";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "SalesClient_default",
+                "SalesClient/{controller}/{action}/{id}",
+                //new { action = "Index", id = UrlParameter.Optional }
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ FirstREST/Areas/SalesOrders/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstREST.Areas.SalesOrders.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /SalesOrders/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
