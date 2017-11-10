8ddb1cdd5f5fb0e7afc61806d14b6a7dcb70cd64
Catarina24 <aniratac.correia@gmail.com>
2017-11-10 12:27:04 +0000

    idk

---
+++ FirstREST/Areas/ProductDetails/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstREST.Areas.ProductDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /ProductDetails/Home/
+
+        public ActionResult Index()
+        {
+            //Artigos
+            List<Lib_Primavera.Model.Artigo> artigos = Lib_Primavera.PriIntegration.ListaArtigos();
+            ViewBag.artigos = artigos;
+
+            return View();
+        }
+
+    }
+}
