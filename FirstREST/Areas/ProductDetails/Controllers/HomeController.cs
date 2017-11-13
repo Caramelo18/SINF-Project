78283dec4cc7ac39fa7d080bb6fda3b844023856
Catarina24 <aniratac.correia@gmail.com>
2017-11-13 01:02:56 +0000

    GetProduct is working ffs

--- FirstREST/Areas/ProductDetails/Controllers/HomeController.cs
+++ FirstREST/Areas/ProductDetails/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web.Mvc;
 using System.Xml;
 using FirstREST.Models;
+using System.Collections.Generic;
 
 namespace FirstREST.Areas.ProductDetails.Controllers
 {
@@ -16,13 +17,17 @@ namespace FirstREST.Areas.ProductDetails.Controllers
 
         public ActionResult Index()
         {
+
+            // The last element of the breadcrumbs list is the current page
+            ViewBag.breadcrumbs = new List<string> { "Home", "Inventory" };
+
             //TODO: change this to only be called once
 
             XmlDocument doc = new XmlDocument();
             doc.Load(Server.MapPath(@"~\Content\saft.xml"));
 
             Saft.SaftIntegration.ParseProducts(doc, db);
-            //Saft.SaftIntegration.addProductsFromPrimaveraToDb(db);
+            Saft.SaftIntegration.addProductsFromPrimaveraToDb(db);
 
             var artigos = from m in db.Product
                           select m;
@@ -32,5 +37,19 @@ namespace FirstREST.Areas.ProductDetails.Controllers
             return View();
         }
 
+        public ActionResult GetProduct(string id)
+        {
+            // The last element of the breadcrumbs list is the current page
+            ViewBag.breadcrumbs = new List<string> { "Home", "Inventory", id};
+
+            System.Diagnostics.Debug.WriteLine("oi, sou um id: " + id);
+            var artigo = from m in db.Product
+                         where m.ProductCode == "A0001"
+                         select m;
+
+            ViewBag.artigo = artigo.ToList();
+
+            return View();
+        }
     }
 }
