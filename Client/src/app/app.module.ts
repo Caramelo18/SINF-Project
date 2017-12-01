d337a3ee6971c8722cc7a4464d530b2f907c6c59
Caramelo18 <fabiocaramelo18@gmail.com>
2017-12-01 00:18:28 +0000

    started overview port, routes fixed

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,8 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module'
+
 import { AppComponent } from './app.component';
 
+import { OverviewComponent } from './overview/overview.component';
 import { ClientsComponent } from './clients/clients.component';
 import { FinancialComponent } from './financial/financial.component';
 import { InventoryComponent } from './inventory/inventory.component';
@@ -13,16 +16,6 @@ import { SuppliersComponent } from './suppliers/suppliers.component';
 
 
 
-
-const appRoutes: Routes = [
-  { path: 'clients', component: ClientsComponent },
-  { path: 'financial', component: FinancialComponent },
-  { path: 'inventory', component: InventoryComponent },
-  { path: 'orders', component: OrdersComponent },
-  { path: 'sales', component: SalesComponent },
-  { path: 'suppliers', component: SuppliersComponent }
-];
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,15 +23,13 @@ const appRoutes: Routes = [
     FinancialComponent,
     InventoryComponent,
     OrdersComponent,
+    OverviewComponent,
     SalesComponent,
     SuppliersComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Client/src/app/overview/overview.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+    selector: 'overview',
+    templateUrl: './overview.component.html',
+    styleUrls: ['./overview.component.css']
+})
+
+export class OverviewComponent implements OnInit{
+
+    constructor(
+    ) { }
+
+    ngOnInit(): void {
+    }
+}
