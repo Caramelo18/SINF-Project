42ff183e86115af15f425a3f60507baeaf21c269
Catarina24 <aniratac.correia@gmail.com>
2017-12-11 17:02:33 +0000

    added captions

--- Client/src/app/inventory/product/product.component.ts
+++ Client/src/app/inventory/product/product.component.ts
@@ -16,10 +16,14 @@ export class ProductComponent implements OnInit {
     private product: string[];
     private sales: string[];
     private docs: string[];
+
     //pie
     public pieChartLabels: string[];
     public pieChartType: string = "pie";
     public pieChartData: number[];
+
+    //line chart
+
 
     constructor(
         private productService: ProductService,
@@ -28,9 +32,7 @@ export class ProductComponent implements OnInit {
     ) { }
 
     public lineChartData: Array<any> = [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-        { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-        { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
+        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }
     ];
     public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
     public lineChartOptions: any = {
